80dcef1cc0807d6f17b676a5a35538189920fae9
zergon321 <maximgradan@gmail.com>
2023-07-25 01:25:16 +0300

    Amortized list added.

--- amortized.go
+++ amortized.go
@@ -1,73 +1,32 @@
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Package list implements a doubly linked list.
-//
-// To iterate over a list (where l is a *List[T]):
-//
-//	for e := l.Front(); e != nil; e = e.Next() {
-//		// do something with e.Value
-//	}
 package list
-
-// Element is an element of a linked list.
-type Element[T any] struct {
-	// Next and previous pointers in the doubly-linked list of elements.
-	// To simplify the implementation, internally a list l is implemented
-	// as a ring, such that &l.root is both the next element of the last
-	// list element (l.Back()) and the previous element of the first list
-	// element (l.Front()).
-	next, prev *Element[T]
-
-	// The list to which this element belongs.
-	list  *List[T]
-	alist *AmortizedList[T]
-
-	// The value stored with this element.
-	Value T
-}
-
-// Next returns the next list element or nil.
-func (e *Element[T]) Next() *Element[T] {
-	if p := e.next; e.list != nil && p != &e.list.root {
-		return p
-	}
-	return nil
-}
-
-// Prev returns the previous list element or nil.
-func (e *Element[T]) Prev() *Element[T] {
-	if p := e.prev; e.list != nil && p != &e.list.root {
-		return p
-	}
-	return nil
-}
 
 // List represents a doubly linked list.
 // The zero value for List is an empty list ready to use.
-type List[T any] struct {
-	root Element[T] // sentinel list element, only &root, root.prev, and root.next are used
-	len  int        // current list length excluding (this) sentinel element
+type AmortizedList[T any] struct {
+	root   Element[T] // sentinel list element, only &root, root.prev, and root.next are used
+	len    int        // current list length excluding (this) sentinel element
+	memory *List[T]
 }
 
 // Init initializes or clears list l.
-func (l *List[T]) Init() *List[T] {
+func (l *AmortizedList[T]) Init() *AmortizedList[T] {
 	l.root.next = &l.root
 	l.root.prev = &l.root
 	l.len = 0
+	l.memory = New[T]()
+
 	return l
 }
 
 // New returns an initialized list.
-func New[T any]() *List[T] { return new(List[T]).Init() }
+func NewAmortized[T any]() *AmortizedList[T] { return new(AmortizedList[T]).Init() }
 
 // Len returns the number of elements of list l.
 // The complexity is O(1).
-func (l *List[T]) Len() int { return l.len }
+func (l *AmortizedList[T]) Len() int { return l.len }
 
 // Front returns the first element of list l or nil if the list is empty.
-func (l *List[T]) Front() *Element[T] {
+func (l *AmortizedList[T]) Front() *Element[T] {
 	if l.len == 0 {
 		return nil
 	}
@@ -75,7 +34,7 @@ func (l *List[T]) Front() *Element[T] {
 }
 
 // Back returns the last element of list l or nil if the list is empty.
-func (l *List[T]) Back() *Element[T] {
+func (l *AmortizedList[T]) Back() *Element[T] {
 	if l.len == 0 {
 		return nil
 	}
@@ -83,40 +42,51 @@ func (l *List[T]) Back() *Element[T] {
 }
 
 // lazyInit lazily initializes a zero List value.
-func (l *List[T]) lazyInit() {
+func (l *AmortizedList[T]) lazyInit() {
 	if l.root.next == nil {
 		l.Init()
 	}
 }
 
 // insert inserts e after at, increments l.len, and returns e.
-func (l *List[T]) insert(e, at *Element[T]) *Element[T] {
+func (l *AmortizedList[T]) insert(e, at *Element[T]) *Element[T] {
 	e.prev = at
 	e.next = at.next
 	e.prev.next = e
 	e.next.prev = e
-	e.list = l
+	e.alist = l
 	l.len++
 	return e
 }
 
 // insertValue is a convenience wrapper for insert(&Element{Value: v}, at).
-func (l *List[T]) insertValue(v T, at *Element[T]) *Element[T] {
-	return l.insert(&Element[T]{Value: v}, at)
+func (l *AmortizedList[T]) insertValue(v T, at *Element[T]) *Element[T] {
+	lastSlot := l.memory.Back()
+
+	if lastSlot == nil {
+		return l.insert(&Element[T]{Value: v}, at)
+	}
+
+	l.memory.Remove(lastSlot)
+	lastSlot.Value = v
+
+	return l.insert(lastSlot, at)
 }
 
 // remove removes e from its list, decrements l.len
-func (l *List[T]) remove(e *Element[T]) {
+func (l *AmortizedList[T]) remove(e *Element[T]) {
 	e.prev.next = e.next
 	e.next.prev = e.prev
 	e.next = nil // avoid memory leaks
 	e.prev = nil // avoid memory leaks
 	e.list = nil
 	l.len--
+
+	l.memory.insert(e, l.memory.Back())
 }
 
 // move moves e to next to at.
-func (l *List[T]) move(e, at *Element[T]) {
+func (l *AmortizedList[T]) move(e, at *Element[T]) {
 	if e == at {
 		return
 	}
@@ -132,8 +102,8 @@ func (l *List[T]) move(e, at *Element[T]) {
 // Remove removes e from l if e is an element of list l.
 // It returns the element value e.Value.
 // The element must not be nil.
-func (l *List[T]) Remove(e *Element[T]) T {
-	if e.list == l {
+func (l *AmortizedList[T]) Remove(e *Element[T]) T {
+	if e.alist == l {
 		// if e.list == l, l must have been initialized when e was inserted
 		// in l or l == nil (e is a zero Element) and l.remove will crash
 		l.remove(e)
@@ -142,13 +112,13 @@ func (l *List[T]) Remove(e *Element[T]) T {
 }
 
 // PushFront inserts a new element e with value v at the front of list l and returns e.
-func (l *List[T]) PushFront(v T) *Element[T] {
+func (l *AmortizedList[T]) PushFront(v T) *Element[T] {
 	l.lazyInit()
 	return l.insertValue(v, &l.root)
 }
 
 // PushBack inserts a new element e with value v at the back of list l and returns e.
-func (l *List[T]) PushBack(v T) *Element[T] {
+func (l *AmortizedList[T]) PushBack(v T) *Element[T] {
 	l.lazyInit()
 	return l.insertValue(v, l.root.prev)
 }
@@ -156,8 +126,8 @@ func (l *List[T]) PushBack(v T) *Element[T] {
 // InsertBefore inserts a new element e with value v immediately before mark and returns e.
 // If mark is not an element of l, the list is not modified.
 // The mark must not be nil.
-func (l *List[T]) InsertBefore(v T, mark *Element[T]) *Element[T] {
-	if mark.list != l {
+func (l *AmortizedList[T]) InsertBefore(v T, mark *Element[T]) *Element[T] {
+	if mark.alist != l {
 		return nil
 	}
 	// see comment in List.Remove about initialization of l
@@ -167,8 +137,8 @@ func (l *List[T]) InsertBefore(v T, mark *Element[T]) *Element[T] {
 // InsertAfter inserts a new element e with value v immediately after mark and returns e.
 // If mark is not an element of l, the list is not modified.
 // The mark must not be nil.
-func (l *List[T]) InsertAfter(v T, mark *Element[T]) *Element[T] {
-	if mark.list != l {
+func (l *AmortizedList[T]) InsertAfter(v T, mark *Element[T]) *Element[T] {
+	if mark.alist != l {
 		return nil
 	}
 	// see comment in List.Remove about initialization of l
@@ -178,8 +148,8 @@ func (l *List[T]) InsertAfter(v T, mark *Element[T]) *Element[T] {
 // MoveToFront moves element e to the front of list l.
 // If e is not an element of l, the list is not modified.
 // The element must not be nil.
-func (l *List[T]) MoveToFront(e *Element[T]) {
-	if e.list != l || l.root.next == e {
+func (l *AmortizedList[T]) MoveToFront(e *Element[T]) {
+	if e.alist != l || l.root.next == e {
 		return
 	}
 	// see comment in List.Remove about initialization of l
@@ -189,8 +159,8 @@ func (l *List[T]) MoveToFront(e *Element[T]) {
 // MoveToBack moves element e to the back of list l.
 // If e is not an element of l, the list is not modified.
 // The element must not be nil.
-func (l *List[T]) MoveToBack(e *Element[T]) {
-	if e.list != l || l.root.prev == e {
+func (l *AmortizedList[T]) MoveToBack(e *Element[T]) {
+	if e.alist != l || l.root.prev == e {
 		return
 	}
 	// see comment in List.Remove about initialization of l
@@ -200,8 +170,8 @@ func (l *List[T]) MoveToBack(e *Element[T]) {
 // MoveBefore moves element e to its new position before mark.
 // If e or mark is not an element of l, or e == mark, the list is not modified.
 // The element and mark must not be nil.
-func (l *List[T]) MoveBefore(e, mark *Element[T]) {
-	if e.list != l || e == mark || mark.list != l {
+func (l *AmortizedList[T]) MoveBefore(e, mark *Element[T]) {
+	if e.alist != l || e == mark || mark.alist != l {
 		return
 	}
 	l.move(e, mark.prev)
@@ -210,8 +180,8 @@ func (l *List[T]) MoveBefore(e, mark *Element[T]) {
 // MoveAfter moves element e to its new position after mark.
 // If e or mark is not an element of l, or e == mark, the list is not modified.
 // The element and mark must not be nil.
-func (l *List[T]) MoveAfter(e, mark *Element[T]) {
-	if e.list != l || e == mark || mark.list != l {
+func (l *AmortizedList[T]) MoveAfter(e, mark *Element[T]) {
+	if e.alist != l || e == mark || mark.alist != l {
 		return
 	}
 	l.move(e, mark)
@@ -219,7 +189,7 @@ func (l *List[T]) MoveAfter(e, mark *Element[T]) {
 
 // PushBackList inserts a copy of another list at the back of list l.
 // The lists l and other may be the same. They must not be nil.
-func (l *List[T]) PushBackList(other *List[T]) {
+func (l *AmortizedList[T]) PushBackList(other *AmortizedList[T]) {
 	l.lazyInit()
 	for i, e := other.Len(), other.Front(); i > 0; i, e = i-1, e.Next() {
 		l.insertValue(e.Value, l.root.prev)
@@ -228,7 +198,7 @@ func (l *List[T]) PushBackList(other *List[T]) {
 
 // PushFrontList inserts a copy of another list at the front of list l.
 // The lists l and other may be the same. They must not be nil.
-func (l *List[T]) PushFrontList(other *List[T]) {
+func (l *AmortizedList[T]) PushFrontList(other *AmortizedList[T]) {
 	l.lazyInit()
 	for i, e := other.Len(), other.Back(); i > 0; i, e = i-1, e.Prev() {
 		l.insertValue(e.Value, &l.root)
